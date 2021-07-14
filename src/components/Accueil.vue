c42069ec2099feb045767290906b85680f0538f9
anis <yahiaoui.anis11@gmail.com>
2021-07-14 02:43:16 +0200

    display list pokemons

--- src/components/Accueil.vue
+++ src/components/Accueil.vue
@@ -1,46 +1,74 @@
 <template>
-  <div>    
-    <div class="container p-4">
-      <div class="row row-cols-1 row-cols-md-5">
-        <pokemon :key="index" v-for="(pokemon, index) in pokemons" :id="index" :pokemon="pokemon"></pokemon>
-      </div>
-    </div>    
+<div>
+    <div class="table-responsive"> 
+      <sorted-table class="table table-striped w-auto" :values="values">        
+        <thead class="thead-dark">
+          <th scope="col">
+            <sort-link name="id"> # </sort-link>
+          </th>
+          <th scope="col">
+            Photo
+          </th>                        
+          <th scope="col">
+            <sort-link name="name"> Nom </sort-link>
+          </th>
+          <th scope="col">
+            Types
+          </th>
+        </thead>
+        <template>
+        <tbody>
+              <tr v-for="(pokemon, index) in pokemons" :key="index">
+                <td> {{pokemon.id + 1}} </td>  
+                <td>
+                  <img class="img-fluid w-5 mx-autos" :src="`${pokemon.sprites.front_default}`" alt="">
+                </td>          
+                  
+                <td>
+                  <router-link class="link" :to="`/pokemon/${pokemon.id + 1}`">
+                    {{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}}
+                  </router-link>  
+                </td>
+                <td>                
+                  <ul :key="i" v-for="(element , i) in pokemon.abilities"> 
+                    <li>{{ element.ability.name }}</li>
+                  </ul>
+                </td>                      
+            </tr>                     
+        </tbody>
+        </template>
+      </sorted-table>     
   </div>
+</div>
 </template>
 
 <script>
-import Pokemon from './Pokemon.vue'
 import axios from 'axios'
 
 export default {
-  name: 'Accueil',
-  components:{       
-    'pokemon' : Pokemon
-  },
+  name: 'Accueil',  
   data(){
     return {
+      values: [],
       pokemons: null,
       loading: true,
       errored: false
     } 
   },
-  mounted(){
-    axios
-    .get('https://pokeapi.co/api/v2/pokemon?limit=20')
-    .then((reponse) => {
-      this.pokemons = reponse.data.results;
-      var i = 1;
-      this.pokemons.forEach(element => {        
-        element.id = i;
-        i++;
-      });
-      console.log(this.pokemons);
-    })
-    .catch(error => {
-        console.log(error)
-        this.errored = true
-    })
-    .finally(() => this.loading = false);
+  async mounted(){
+    let pokemons = []
+    for (var i=0; i<20; i++){
+      let url = 'https://pokeapi.co/api/v2/pokemon/' + (i+1);      
+      await axios.get(url).then((response) => {        
+        response.data.id = i;
+        pokemons[i] = response.data;        
+      }).catch(function (error) {        
+        console.log(error);        
+      }).finally(() => this.loading = false);      
+    }
+    this.pokemons = pokemons;
+    this.values = pokemons;
+    console.log(this.values);
   }
 }
 </script>
